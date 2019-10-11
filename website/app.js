67540fea1353a0330ad0f97ab16446f982e2b443
Chinedu Daniel <chinedudaniel7@gmail.com>
2019-10-11 07:19:52 +0100

    Initial commit

---
+++ website/app.js
@@ -0,0 +1,15 @@
+/* Global Variables */
+
+// Create a new date instance dynamically with JS
+// let d = new Date();
+// let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+(function() {
+  const baseUrl = "https://api.openweathermap.org/data/2.5/waether";
+  const apiKey = "80fd0c28caaeba68344b22209ee521c1";
+
+  const zipElem = document.getElementById("zip");
+  const feelingsElem = document.getElementById("feelings");
+  const generateBtn = document.getElementById("generate");
+
+  generateBtn.addEventListener("click", () => {});
+})();
